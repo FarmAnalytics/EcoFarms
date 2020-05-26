55f7859583ccdf624c09f681d3b7e30bba40fc77
phbasin <pierre-henri.basin@orange.fr>
2020-05-26 16:57:38 +0200

    working on seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_103939) do
+ActiveRecord::Schema.define(version: 2020_05_26_144255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_103939) do
     t.text "interest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "claps", force: :cascade do |t|

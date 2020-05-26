0ee81de199311c08a90a1025ee5d89dae0ae004e
phbasin <pierre-henri.basin@orange.fr>
2020-05-26 16:41:10 +0200

    working on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+puts 'Cleaning database...'
+Farm.destroy_all
+
+puts 'Creating farms...'
+# csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+# filepath    = 'seeds.csv'
+load_farms('seeds.csv')
+
+puts "Finished!"
+
+private
+
+def true?(obj)
+  obj.to_s.downcase == "true"
+end
+
+def load_farms(file_path)
+  farms = []
+  categories = []
+  criterions = []
+  CSV.foreach(file_path) do |row|
+    farms << { name: row[0], address: row[1], sector: row[3], private: true?(row[4]), description: row[5], url: row[10], cultures: row[13], surface: row[14].to_i, employees: row[15].to_i }
+    
+  end
+  farms
+end
+
+
+
+#   user = User.new(
+#     password: "alpha0101",
+#     first_name: 'Elon',
+#     last_name: 'Musk',
+#     phone_number: '0150504545',
+#     email: 'elonm94@mail.com')  
+#   user.save
+#   puts "User #{user.email} created"
+
+# 20.times do
+
+#   plug = Plug.new(
+#       address: a.sample,
+#       power: [16, 30, 40, 70].sample,
+#       ac_dc: ["AC", "DC"].sample,
+#       type_plug: ['Type 2', 'Type 3C', 'Type 1', 'CHAdeMO', 'Combo CCS', 'P17' ].sample,
+#       price: rand(8..20)
+#   )
+ 
+#     plug.user = user
+#     plug.save!
+#     puts "Plug \"#{plug.type_plug} - #{plug.power}\"created"
+
+# end
+
+# puts'Creating bookings...'
+# 20.times do 
+#   booking = Booking.new 
+#   booking.user = User.all.sample 
+#   plug = Plug.all.sample
+#   booking.plug = plug
+#   puts "Booking for #{booking.user} created"
+# end

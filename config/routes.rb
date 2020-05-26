ba16a1a7fb5b0d74ddb67bf79f822c9dc53829b0
phbasin <pierre-henri.basin@orange.fr>
2020-05-26 13:12:01 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :farms do
+    resources :claps, only: [ :create ]
+    resources :scorings, only: [ :create ]
+  end
+  
+  resources :likes, only: [ :create ]
+  
+  resources :users, only: [ :show ] do
+    resources :lists
+  end
+
+  # resources :plugs, only: [ :index, :new, :create, :show, :update, :edit, :destroy ] do 
+    # resources :bookings, only: [ :new, :create, :update ] 
+  # end
 end

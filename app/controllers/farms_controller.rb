7e01d2cf7e76191a5f90439a7be1265b74c85b84
Louis Mauriac <mauriaclouis@hotmail.com>
2020-05-26 17:15:56 +0200

    created a view for index with geocoder and mapbox

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,4 +1,11 @@
 class FarmsController < ApplicationController
   def index
+    @farms = policy_scope(Farm)
+    @markers = @farms.map do |farm|
+      {
+        lat: farm.latitude,
+        lng: farm.longitude
+      }
+    end
   end
 end

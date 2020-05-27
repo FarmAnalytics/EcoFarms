060074a2f02b0ba6947f85385b935647106c37d5
louismau <62843536+louismau@users.noreply.github.com>
2020-05-27 17:18:46 +0200

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,6 @@ import { initSelect2 } from '../plugins/init_select2';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { initSelect2 } from '../components/dashboard'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:

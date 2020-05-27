9e2f7691e4c334d36aa36a342c63c13fbf520382
Louis Mauriac <mauriaclouis@hotmail.com>
2020-05-27 22:03:33 +0200

    playing with mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,15 +4,22 @@ const mapElement = document.getElementById('map');
 
 const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  return new mapboxgl.Map({
+  const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mapbox/streets-v10',
   });
+ return map
 };
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
-    const mark = new mapboxgl.Marker()
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '25px';
+    element.style.height = '25px';
+    const mark = new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
     const el = mark._element
@@ -23,9 +30,16 @@ const addMarkersToMap = (map, markers) => {
 };
 
 const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  if (mapElement.dataset.longitude === "") {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  
+   } else {
+    const coordinates = []
+    coordinates.push(parseFloat(mapElement.dataset.longitude));
+    coordinates.push(parseFloat(mapElement.dataset.latitude));
+  }
 };
 
 const initMapbox = () => {
@@ -33,15 +47,22 @@ const initMapbox = () => {
     const map = buildMap();
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
-    const popups = document.querySelector('.mapboxgl-marker');
-      // popups.forEach((popup) => {
-      //   console.log(popup);
-      //   popup.addEventListener('click', (event) => 
-      //     mapElement.insertAdjacentHTML('beforeend', marker.infoWindow)
-      //   );
-    //   }
-    // })
+    // fitMapToMarkers(map, markers);
+    if (mapElement.dataset.longitude === "") {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+    
+     } else {
+      const coordinates = []
+      coordinates.push(parseFloat(mapElement.dataset.latitude));
+      coordinates.push(parseFloat(mapElement.dataset.longitude));
+      console.log(coordinates)
+      map.setCenter(coordinates);
+      console.log(map.getCenter());
+
+      map.setZoom(10);
+    }
   }
 };
 

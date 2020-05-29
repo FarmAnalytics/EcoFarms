2843e80a4007cafb0031f54b01dc5c3045718ece
phbasin <pierre-henri.basin@orange.fr>
2020-05-29 15:24:27 +0200

    update

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,4 +2,12 @@ class Farm < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  def total
+    scores = []
+    Scoring.where(farm_id: self.id).each do |note| 
+      scores << note.score
+    end
+    scores.sum / scores.count
+  end
 end

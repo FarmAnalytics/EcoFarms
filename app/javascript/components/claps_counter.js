740070337be555e644c4723e942c44c5013e523a
phbasin <pierre-henri.basin@orange.fr>
2020-06-02 10:15:01 +0200

    improving the claps counter

--- app/javascript/components/claps_counter.js
+++ app/javascript/components/claps_counter.js
@@ -1,26 +1,34 @@
-const onClapButtonClick = (event) => {
+import Rails from "@rails/ujs";
+
+const onClapButtonClick = event => {
   event.preventDefault();
-  const form = document.querySelector('.formclapbutton')
-  Rails.fire(form, 'submit');
-  fetch(window.location.href + '/count_clap', { headers: { accept: 'application/json' } } )
+  const form = document.querySelector(".formclapbutton");
+  Rails.fire(form, "submit");
+};
+
+const updateCount = () => {
+  fetch(window.location.href + "/count_clap", {
+    headers: { accept: "application/json" }
+  })
     .then(response => response.json())
-    .then((data) => {
-      console.log(data)
-      const counter = document.querySelector('.rating-num');
+    .then(data => {
+      const counter = document.querySelector(".rating-num");
       counter.innerHTML = data.clapsCount;
-    })
-}
+    });
+};
 
 const countClaps = () => {
-  const clapButton = document.querySelector('.clapbutton');
-  clapButton.addEventListener('click', onClapButtonClick);
-}
+  const clapButton = document.querySelector(".clapbutton");
+  clapButton.addEventListener("click", onClapButtonClick);
+
+  setInterval(updateCount, 1500);
+};
 
 const initClapsCounter = () => {
-  const counter = document.querySelector('.rating-num');
+  const counter = document.querySelector(".rating-num");
   if (counter) {
     countClaps();
   }
-}
+};
 
 export { initClapsCounter };

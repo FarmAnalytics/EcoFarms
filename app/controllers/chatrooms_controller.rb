978a79a829be89a5415f1d8e83922df0cd59224e
louismau <62843536+louismau@users.noreply.github.com>
2020-06-03 22:17:01 +0200

    Update chatrooms_controller.rb

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,7 +4,6 @@ class ChatroomsController < ApplicationController
     skip_authorization
     @user = current_user
     @chatrooms = policy_scope(Chatroom).where(user_id: @user.id)
-    @lists = List.all
   end
 
   def show
